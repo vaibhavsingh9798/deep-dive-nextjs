ddc5782a874872d991f1f2d882e369c4bf4bea01
Vaibhav <vaibhavsinghvrajpot@gmail.com>
2024-03-11 11:00:20 +0530

    unmatched route redirect

--- app/meetups/[meetupId]/page.tsx
+++ app/meetups/[meetupId]/page.tsx
@@ -1,12 +1,19 @@
 
 import MeetupItemDetails from "@/components/meetups/MeetupItemDetails"
 import meetups from '@/app/lib/meetup-list'
-export default function MeetupDetails({params}:{params:{meetupId:String}}){
+import { redirect } from "next/navigation";
 
-    const id  = params.meetupId;
+export default function MeetupDetails({params}:{params:{meetupId:any}}){
+
+    let id  = params.meetupId;
     const item = meetups.filter((meetup) => meetup.id == id)
+     
+    if(!item.length){
+       redirect('/meetups')
+    }
     return(
         <MeetupItemDetails  meetup={item[0]}/>
     )
 }
+
 
